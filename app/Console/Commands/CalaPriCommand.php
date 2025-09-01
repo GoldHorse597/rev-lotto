e478145a1bb1663e0fc130076c88678a1063edbc
GoldHorse597 <goldhorse597@gmail.com>
2025-09-01 18:23:09 +0900

    프리미엄 로또 추가

--- app/Console/Commands/CalaPriCommand.php
+++ app/Console/Commands/CalaPriCommand.php
@@ -3,7 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Http\Controllers\LottoController;
+use App\Http\Controllers\Admin\LottoController;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 
